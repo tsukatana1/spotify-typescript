2fe6bc215ef9c44dba1008ed8bb08e45fad455e2
tsukatana1 <daviddlon4949@gmail.com>
2022-01-09 15:54:08 -0800

    new changes

---
+++ src/utils.ts
@@ -0,0 +1,22 @@
+export const _ = {
+    getAlbumURL: (id: string) => `https://api.spotify.com/v1/albums/${id}`,
+    
+    getMultiAlbums: (id: string) => `https://api.spotify.com/v1/albums?ids=${id}`,
+
+    getAlbumTracksURL: (id: string) => `https://api.spotify.com/v1/albums/${id}/tracks`,
+
+    getOauthURL: () => `https://accounts.spotify.com/api/token`,
+
+    searchURL: (type: string, q: string) => `https://api.spotify.com/v1/search?type=${type}&q=${q}`,
+    
+    getTrackURL: (id: string) => `https://api.spotify.com/v1/tracks/${id}`
+};
+
+export enum SearchTypes {
+    ALBUM = "album",
+    ARTIST = "artist",
+    PLAYLIST = "playlist",
+    TRACK = "track",
+    SHOW = "show",
+    EPISODE = "episode"
+};
